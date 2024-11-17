a883ced02cd19a5854255f464a2e156d429d9210
Rubens dos Santos Chagas <rubens_shippo@hotmail.com>
2024-11-17 14:58:39 -0300

    commit correto

--- Assets/Scripts/Interfaces/Iinteragivel.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Iinteragivel
-{
-    // Start is called before the first frame update
-    public void Interagir() { }
-}
